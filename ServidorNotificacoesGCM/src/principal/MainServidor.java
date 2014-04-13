5ca7bca40c8feea905b08b598bc045e8fc239472
Alexandre Mota <alexandremota.es@outlook.com>
2014-04-13 07:29:24 -0300

    Criando projetos do cliente e servidor GCM

---
+++ ServidorNotificacoesGCM/src/principal/MainServidor.java
@@ -0,0 +1,13 @@
+package principal;
+
+public class MainServidor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ServidorGCM servidorGCM = new ServidorGCM();
+		servidorGCM.enviarMensagem("", "");
+		
+	}
+
+}
