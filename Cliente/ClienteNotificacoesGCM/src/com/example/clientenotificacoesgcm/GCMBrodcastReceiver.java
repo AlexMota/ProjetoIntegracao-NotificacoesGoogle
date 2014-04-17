40c3e3856b2beabdd089da2f42c1ea8bdefceb05
Alexandre Mota <alexandremota.es@outlook.com>
2014-04-17 17:32:44 -0300

    Criando a classe GCMBroadCastReceiver

---
+++ Cliente/ClienteNotificacoesGCM/src/com/example/clientenotificacoesgcm/GCMBrodcastReceiver.java
@@ -0,0 +1,12 @@
+package com.example.clientenotificacoesgcm;
+
+import android.content.Intent;
+
+public class GCMBrodcastReceiver {
+
+	public static void completeWakefulIntent(Intent intent) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- Cliente/ClienteNotificacoesGCM/src/com/example/clientenotificacoesgcm/ServicoGCM.java
+++ Cliente/ClienteNotificacoesGCM/src/com/example/clientenotificacoesgcm/ServicoGCM.java
@@ -1,12 +1,23 @@
 package com.example.clientenotificacoesgcm;
 
-import com.google.android.gms.gcm.GoogleCloudMessaging;
-
 import android.app.IntentService;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
 
-public class ServicoGCM extends IntentService{
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+
+public class ServicoGCM extends IntentService {
+
+	private static final int NOTIFICATION_ID = 1;
+	private static final String TAG = null;
+	private NotificationManager mNotificationManager;
+	NotificationCompat.Builder builder;
 
 	public ServicoGCM(String name) {
 		super(name);
@@ -16,21 +27,60 @@ public class ServicoGCM extends IntentService{
 	@Override
 	protected void onHandleIntent(Intent intent) {
 		// TODO Auto-generated method stub
-		
+
 		Bundle extras = intent.getExtras();
 		GoogleCloudMessaging gcm = GoogleCloudMessaging.getInstance(this);
-		
+
 		String messageType = gcm.getMessageType(intent);
-		
-		if(!extras.isEmpty()){
-			
-			if(GoogleCloudMessaging.MESSAGE_TYPE_SEND_ERROR.equals(messageType)){
+
+		if (!extras.isEmpty()) {
+
+			if (GoogleCloudMessaging.MESSAGE_TYPE_SEND_ERROR
+					.equals(messageType)) {
+				sendNotification("Send error: "+extras.toString());
+			} else if(GoogleCloudMessaging.MESSAGE_TYPE_DELETED.equals(messageType)){
+				sendNotification("Deleted messages on server: "+extras.toString());
+			} else if(GoogleCloudMessaging.MESSAGE_TYPE_MESSAGE.equals(messageType)){
+				
+				for(int i = 0; i < 5; i++){
+					Log.i(TAG, "Working ..."+(i+1)+"/5@"+SystemClock.elapsedRealtime());
+				
+					try {
+						Thread.sleep(5000);
+					} catch (InterruptedException e) {
+						// TODO: handle exception
+					}
+					
+				}
+				Log.i(TAG, "Completed work @ "+SystemClock.elapsedRealtime());
+				
+				sendNotification("Received: "+extras.toString());
+				Log.i(TAG, "Received: "+extras.toString());
 				
 			}
-			
+
 		}
-		
+		GCMBrodcastReceiver.completeWakefulIntent(intent);
+
 	}
-	
+
+	private void sendNotification(String msg) {
+
+		mNotificationManager = (NotificationManager) this
+				.getSystemService(Context.NOTIFICATION_SERVICE);
+
+		PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
+				new Intent(this, DemoActivity.class), 0);
+
+		NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(
+				this).setSmallIcon(R.drawable.ic_launcher)
+				.setContentTitle("GCM Notificacao")
+				.setStyle(new NotificationCompat.BigTextStyle().bigText(msg))
+				.setContentText(msg);
+		
+		mBuilder.setContentIntent(contentIntent);
+		mNotificationManager.notify(NOTIFICATION_ID, mBuilder.build());
+
+	}
 
 }
