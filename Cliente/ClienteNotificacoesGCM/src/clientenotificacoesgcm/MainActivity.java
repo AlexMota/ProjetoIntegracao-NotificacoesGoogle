83fcce606b9780cc3ebe7d968e2bb347ab1d8b7e
Alexandre Mota <alexandremota.es@outlook.com>
2014-04-24 02:48:55 -0300

    Concluindo metodo getGcmPreferences

--- Cliente/ClienteNotificacoesGCM/src/clientenotificacoesgcm/MainActivity.java
+++ Cliente/ClienteNotificacoesGCM/src/clientenotificacoesgcm/MainActivity.java
@@ -1,15 +1,23 @@
 package clientenotificacoesgcm;
 
+import DemoActivity;
+
+import java.sql.Connection;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import com.example.clientenotificacoesgcm.R;
+import com.google.android.gms.auth.GooglePlayServicesAvailabilityException;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.gcm.GoogleCloudMessaging;
 
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -20,35 +28,73 @@ import android.os.Build;
 
 public class MainActivity extends ActionBarActivity {
 
+	public static final String EXTRA_MESSAGE = "message";
+	public static final String PROPERTY_REG_ID = "registration_id";
+	private static final String PROPERTY_APP_VERSION = "appVersion";
+	private static final int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
 
-    public static final String EXTRA_MESSAGE = "message";
-    public static final String PROPERTY_REG_ID = "registration_id";
-    private static final String PROPERTY_APP_VERSION = "appVersion";
-    private static final int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
-    
-    String SENDER_ID = "Your-Sender-ID";
-    
-    TextView mDisplay;
-    GoogleCloudMessaging gcm;
-    AtomicInteger msgId =  new AtomicInteger();
-    Context context;
-    
-    String regId;
+	String SENDER_ID = "Your-Sender-ID";
+	static final String TAG = "GCM Demo";
 
-	
+	TextView mDisplay;
+	GoogleCloudMessaging gcm;
+	AtomicInteger msgId = new AtomicInteger();
+	Context context;
+
+	String regId;
+
+	private boolean checkPlayServices() {
+		// TODO Auto-generated method stub
+
+		int resultCode = GooglePlayServicesUtil
+				.isGooglePlayServicesAvailable(this);
+
+		if (resultCode != ConnectionResult.SUCCESS) {
+
+			if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
+				GooglePlayServicesUtil.getErrorDialog(resultCode, this,
+						PLAY_SERVICES_RESOLUTION_REQUEST).show();
+			} else {
+
+				Log.i(TAG, "This device is not supported.");
+				finish();
+
+			}
+			return false;
+
+		}
+
+		return true;
+	}
+
+	private SharedPreferences getGcmPreferences(Context context) {
+
+		return getSharedPreferences(MainActivity.class.getSimpleName(),
+				Context.MODE_PRIVATE);
+	}
+
+	private String getRegistrationId(Context context) {
+		
+		final SharedPreferences prefs = getGcmPreferences(context);
+		
+		return null;
+	}
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		mDisplay = (TextView) findViewById(R.id.display);
-		
-		context = getApplicationContext();
-		
-		if(checkPlayServices()){
-			
-		}
-		
-	}
 
+		context = getApplicationContext();
+
+		if (checkPlayServices()) {
+
+			gcm = GoogleCloudMessaging.getInstance(this);
+			regId = getRegistrationId(context);
+
+		}
+
+	}
 
 }
